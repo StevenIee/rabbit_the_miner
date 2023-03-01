1b994f116d37b2485baaba11f0266a4739017d72
Jeonghyun lee <jeonghyun.lee@snu.ac.kr>
2023-03-01 10:19:37 +0900

    LJH_connection.py update

--- Connection.py
+++ Connection.py
@@ -2,20 +2,23 @@
 """
 Created on Wed Feb 15 16:07:41 2023
 
-@author: JISU
+@author: JISU babo
 """
+import rpyc
+import time
 
-from datetime import date
-from NF_laxtha_v2 import NNFX2_FAA
-
-class Baseline:
-    
-    def __init__(self, player_id, player_session, player_datafile):
+class Connect:
+    def __init__(self):
+        self.rpy = [];
         
-        Base = NNFX2_FAA(player_datafile)
-        Base.set_EEGcalc(5, 0.1);
-        self.faa_all = Base.baseline_recording(180, 1000, [8, 13]) # <- duration set
+    def check(self):
+        try:
+            self.rpy = rpyc.connect("localhost", 18861, config = {'allow_all_attrs': True, 'allow_pickle':True})
+            return self.rpy
+    
+        except ConnectionRefusedError:
+            print('Please run a servier_APP first!!')
+            time.sleep(1)
+            empty_rpy = []
+            return empty_rpy
 
-
-
-    # def 
